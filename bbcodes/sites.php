6e799ce472d6d03f91fbee9940342ad704df639c
alexonbstudio <alexonbstudio@gmail.com>
2020-08-07 13:29:56 +0200

    Create sites.php

---
+++ bbcodes/sites.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @package	Plugin for Joomla!
+ * @subpackage  plg_shortcode
+ * @version	4.1.1
+ * @author	AlexonBalangue.me
+ * @copyright	(C) 2012-2015 Alexon Balangue. All rights reserved.
+ * @license	GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+//no direct accees
+defined ('_JEXEC') or die('resticted aceess');
+
+if(!function_exists('sites_sc')) {
+
+	function badgelxw_sc( $atts, $content) {
+		extract(bbcodes_atts(array(
+				'url' => ''
+		 ), $atts));
+		 
+		$urls = ($url !='') ? $url : JURI::current();
+		
+		return $urls.' '.$content;
+	}
+		
+	add_bbcodes( 'sites', 'sites_sc' );
+}
