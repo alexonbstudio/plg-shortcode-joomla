cb67a0347e188bee6bc6e55d850280b27f9c3ed3
AlexonbStudio.fr <65239996+alexonbstudio@users.noreply.github.com>
2020-09-25 18:49:54 +0200

    Update shortcode.php
    not compatible error

--- shortcode.php
+++ shortcode.php
@@ -10,58 +10,28 @@
 
 //no direct accees
 defined ('_JEXEC') or die;
+if(!defined('DS')) define('DS', DIRECTORY_SEPARATOR);# Add this code For Joomla 3.3.4+
 
-#USING
-#use Joomla\CMS\Helper\ModuleHelper;
-#use Joomla\CMS\HTML\HTMLHelper;
-use Joomla\CMS\Factory;
-use Joomla\CMS\Document;
-use Joomla\CMS\Uri\Uri;
-use Joomla\CMS\Language;
-use Joomla\CMS\Plugin\CMSPlugin;
-#use Joomla\CMS\User;
-#use Joomla\Component\Finder\Administrator\Indexer\Query;
-#use Joomla\Utilities\ArrayHelper;
+jimport('joomla.plugin.plugin');
+jimport( 'joomla.event.plugin' );
 
-/*
-$configs = Factory::getConfig();
-$langs = Factory::getLanguage();
-#$langs = Factory::getLanguage(); #LanguageHelper::exists($lang)
-$users = Factory::getUser();
-$roots = Uri::root();
-$base = Uri::base();
-$current = Uri::current();
-*/
-//if(!defined('DS')) define('DS', DIRECTORY_SEPARATOR);# Add this code For Joomla 3.3.4+
-
-//jimport('joomla.plugin.plugin');
-//jimport('joomla.event.plugin');
-
-class PlgSystemShortcode extends CMSPlugin #JPlugin
+class PlgSystemShortcode extends JPlugin
 {
-	/**
-	 * Application object
-	 *
-	 * @var    \Joomla\CMS\Application\CMSApplication
-	 * @since  4.0.0
-	 */
-	protected $app;
-	
-	//protected $autoloadLanguage = true;
+	protected $autoloadLanguage = true;
 	
 	//public function plgSystemShortcode(&$subject,$config)
-	//public function __construct(&$subject,$config)
-	//{
-	//	parent::__construct($subject,$config); 
-	//	//$this->loadLanguage();
-	//	
-	//}
+	public function __construct(&$subject,$config)
+	{
+		parent::__construct($subject,$config); 
+		//$this->loadLanguage();
+		
+	}
 	
     public function onAfterInitialise()
     {
-        $shortcode_path = JPATH_PLUGINS.'/system/shortcode/wp/shortcode.php';
+        $shortcode_path = JPATH_PLUGINS.DS.'system'.DS.'shortcode'.DS.'wp'.DS.'shortcode.php';
         if (file_exists($shortcode_path)) {
-            require_once $shortcode_path;
+            require_once($shortcode_path);
             Shortcodes::getInstance()->loadShortcodesOverwrite()->importbbcodeFiles();
         }
 
@@ -69,12 +39,14 @@ class PlgSystemShortcode extends CMSPlugin #JPlugin
 
     public function onAfterRender()
     {
-		#GET LIBS
-		$app = Factory::getApplication();
-		$doc = Factory::getDocument();
-		
-		if( $this->app->isClient('site') ) {
-			$data = $this->app->getBody(); 
+		$app = JFactory::getApplication();
+		$docs = JFactory::getDocument();
+		if( $app->isAdmin() ) {
+			$data = JResponse::getBody();
+			JResponse::setBody($data);			
+					
+		} else {	
+			$data = JResponse::getBody(); 
 
 			$new_html_data = '';
 	
@@ -82,14 +54,9 @@ class PlgSystemShortcode extends CMSPlugin #JPlugin
 			$data = do_bbcodes($data); 
 			$data = str_replace('</html>', $new_html_data . "\n</html>", $data);
 
-			$this->app->setBody($data);	
-			$doc->addStyleDeclaration('.grade{text-align:center;margin:15px auto;width:72px;height:72px;font-size:50px;line-height:72px;font-weight:400;color:#fff}.grade-a{background-color:#00A500}.grade-b{background-color:#68D035}.grade-c{background-color:#F8CF00}.grade-d{background-color:#FFA901}.grade-e{background-color:#FF7701}.grade-f,.grade-m,.grade-t,.grade-unknown{background-color:#FF4D41}');
-					
-		} else {	
-			#$data = JResponse::getBody(); #Joomla =< 3.9
-			$data = $this->app->getBody()
-			$this->app->setBody($data);		
+			JResponse::setBody($data);
 		}
+		$docs->addStyleDeclaration('.grade{text-align:center;margin:15px auto;width:72px;height:72px;font-size:50px;line-height:72px;font-weight:400;color:#fff}.grade-a{background-color:#00A500}.grade-b{background-color:#68D035}.grade-c{background-color:#F8CF00}.grade-d{background-color:#FFA901}.grade-e{background-color:#FF7701}.grade-f,.grade-m,.grade-t,.grade-unknown{background-color:#FF4D41}');
 
 	}
 }
