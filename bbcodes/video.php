13cd24a1eca69d54cc5c54a346ea7a7bed58300c
AlexonbStudio.fr <65239996+alexonbstudio@users.noreply.github.com>
2020-09-30 17:49:13 +0200

    Update video.php

--- bbcodes/video.php
+++ bbcodes/video.php
@@ -103,82 +103,6 @@ if(!function_exists('vimeo_sc')) {
 	add_bbcodes( 'vimeo', 'vimeo_sc' );
 }
 
-if(!function_exists('vine_sc')) {
-
-	function vine_sc( $atts, $content) {
-		extract(bbcodes_atts(array(
-				'width' => '',
-				'height' => '',
-				'source' => ''
-		 ), $atts));
-		 
-		$option = ($width !='') ? ' width="'.$width.'"' : ' width="100%"';
-		$option .= ($height !='') ? ' height="'.$height.'"' : ' height="450"';
-		$option .= ($source !='') ? ' src="://vine.co/v/'.$source.'/embed/simple"' : '';
-
-		return '<iframe'.$option.' frameborder="0">'.do_bbcodes($content).'</iframe>';
-	}
-		
-	add_bbcodes( 'vine', 'vine_sc' );
-}
-
-if(!function_exists('veevr_sc')) {
-
-	function veevr_sc( $atts, $content) {
-		extract(bbcodes_atts(array(
-				'width' => '',
-				'height' => '',
-				'source' => ''
-		 ), $atts));
-		 
-		$option = ($width !='') ? ' width="'.$width.'"' : ' width="100%"';
-		$option .= ($height !='') ? ' height="'.$height.'"' : ' height="450"';
-		$option .= ($source !='') ? ' src="http://embed.veevr.com/video/'.$source.'"' : '';
-
-		return '<iframe'.$option.' frameborder="0" scrolling="no" webkitallowfullscreen mozallowfullscreen allowfullscreen>'.do_bbcodes($content).'</iframe>';
-	}
-		
-	add_bbcodes( 'veevr', 'veevr_sc' );
-}
-
-if(!function_exists('ustream_sc')) {
-
-	function ustream_sc( $atts, $content) {
-		extract(bbcodes_atts(array(
-				'width' => '',
-				'height' => '',
-				'source' => ''
-		 ), $atts));
-		 
-		$option = ($width !='') ? ' width="'.$width.'"' : ' width="100%"';
-		$option .= ($height !='') ? ' height="'.$height.'"' : ' height="450"';
-		$option .= ($source !='') ? ' src="http://www.ustream.tv/embed/'.$source.'?v=3&amp;wmode=direct"' : '';
-
-		return '<iframe'.$option.' frameborder="0" scrolling="no" webkitallowfullscreen mozallowfullscreen allowfullscreen>'.do_bbcodes($content).'</iframe>';
-	}
-		
-	add_bbcodes( 'ustream', 'ustream_sc' );
-}
-
-if(!function_exists('youmaker_sc')) {
-
-	function youmaker_sc( $atts, $content) {
-		extract(bbcodes_atts(array(
-				'width' => '',
-				'height' => '',
-				'source' => ''
-		 ), $atts));
-		 
-		$option = ($width !='') ? ' width="'.$width.'"' : ' width="100%"';
-		$option .= ($height !='') ? ' height="'.$height.'"' : ' height="450"';
-		$option .= ($source !='') ? ' src="http://www.youmaker.com/video/showAFlvEmbed-b5-'.$source.'.html"' : '';
-
-		return '<iframe'.$option.' frameborder="0" allowfullscreen>'.do_bbcodes($content).'</iframe>';
-	}
-		
-	add_bbcodes( 'youmaker', 'youmaker_sc' );
-}
-
 if(!function_exists('youku_sc')) {
 
 	function youku_sc( $atts, $content) {
@@ -198,119 +122,6 @@ if(!function_exists('youku_sc')) {
 	add_bbcodes( 'youku', 'youku_sc' );
 }
 
-if(!function_exists('yfrog_sc')) {
-
-	function yfrog_sc( $atts, $content) {
-		extract(bbcodes_atts(array(
-				'width' => '',
-				'height' => '',
-				'source' => ''
-		 ), $atts));
-		 
-		$option = ($width !='') ? ' width="'.$width.'"' : ' width="100%"';
-		$option .= ($height !='') ? ' height="'.$height.'"' : ' height="450"';
-		$option .= ($source !='') ? ' src="http://yfrog.com/'.$source.':embed"' : '';
-
-		return '<iframe'.$option.' frameborder="0" allowfullscreen>'.do_bbcodes($content).'</iframe>';
-	}
-		
-	add_bbcodes( 'yfrog', 'yfrog_sc' );
-}
-
-if(!function_exists('yahoo_sc')) {
-
-	function yahoo_sc( $atts, $content) {
-		extract(bbcodes_atts(array(
-				'width' => '',
-				'height' => '',
-				'source' => ''
-		 ), $atts));
-		 
-		$option = ($width !='') ? ' width="'.$width.'"' : ' width="100%"';
-		$option .= ($height !='') ? ' height="'.$height.'"' : ' height="450"';
-		$option .= ($source !='') ? ' src="//screen.yahoo.com/'.$source.'.html?format=embed"' : '';
-
-		return '<iframe'.$option.' frameborder="0" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" allowtransparency="true">'.do_bbcodes($content).'</iframe>';
-	}
-		
-	add_bbcodes( 'yahoo', 'yahoo_sc' );
-}
-
-if(!function_exists('veoh_sc')) {
-
-	function veoh_sc( $atts, $content) {
-		extract(bbcodes_atts(array(
-				'width' => '',
-				'height' => '',
-				'source' => ''
-		 ), $atts));
-		 
-		$option = ($width !='') ? ' width="'.$width.'"' : ' width="100%"';
-		$option .= ($height !='') ? ' height="'.$height.'"' : ' height="450"';
-		$sources = ($source !='') ? '"http://www.veoh.com/swf/webplayer/WebPlayer.swf?version=AFrontend.5.7.0.1504&permalinkId='.$source.'&player=videodetailsembedded&videoAutoPlay=0&id=anonymous"' : '';
-
-		return '<object'.$option.' id="veohFlashPlayer" name="veohFlashPlayer"><param name="movie" value'.$sources.'</param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src='.$sources.' type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true"'.$option.' id="veohFlashPlayerEmbed" name="veohFlashPlayerEmbed"></embed></object>'.$content;
-	}
-		
-	add_bbcodes( 'veoh', 'veoh_sc' );
-}
-
-if(!function_exists('vbox7_sc')) {
-
-	function vbox7_sc( $atts, $content) {
-		extract(bbcodes_atts(array(
-				'width' => '',
-				'height' => '',
-				'source' => ''
-		 ), $atts));
-		 
-		$option = ($width !='') ? ' width="'.$width.'"' : ' width="100%"';
-		$option .= ($height !='') ? ' height="'.$height.'"' : ' height="450"';
-		$option .= ($source !='') ? ' src="http://vbox7.com/emb/external.php?'.$source.'"' : '';
-
-		return '<iframe'.$option.' frameborder="0" allowfullscreen>'.do_bbcodes($content).'</iframe>';
-	}
-		
-	add_bbcodes( 'vbox7', 'vbox7_sc' );
-}
-
-if(!function_exists('dotsub_sc')) {
-
-	function dotsub_sc( $atts, $content) {
-		extract(bbcodes_atts(array(
-				'width' => '',
-				'height' => '',
-				'source' => ''
-		 ), $atts));
-		 
-		$option = ($width !='') ? ' width="'.$width.'"' : ' width="100%"';
-		$option .= ($height !='') ? ' height="'.$height.'"' : ' height="450"';
-		$option .= ($source !='') ? ' src="//dotsub.com/media/'.$source.'/embed/"' : '';
-
-		return '<iframe'.$option.' frameborder="0" allowfullscreen>'.do_bbcodes($content).'</iframe>';
-	}
-		
-	add_bbcodes( 'dotsub', 'dotsub_sc' );
-}
-
-if(!function_exists('gametrailers_sc')) {
-
-	function gametrailers_sc( $atts, $content) {
-		extract(bbcodes_atts(array(
-				'width' => '',
-				'height' => '',
-				'source' => ''
-		 ), $atts));
-		 
-		$option = ($width !='') ? ' width="'.$width.'"' : ' width="100%"';
-		$option .= ($height !='') ? ' height="'.$height.'"' : ' height="450"';
-		$option .= ($source !='') ? ' src="http://media.mtvnservices.com/embed/mgid:arc:video:gametrailers.com:'.$source.'"' : '';
-
-		return '<iframe'.$option.' frameborder="0" allowfullscreen>'.do_bbcodes($content).'</iframe>';
-	}
-		
-	add_bbcodes( 'gametrailers', 'gametrailers_sc' );
-}
 
 if(!function_exists('gloria_sc')) {
 
@@ -331,62 +142,6 @@ if(!function_exists('gloria_sc')) {
 	add_bbcodes( 'gloria', 'gloria_sc' );
 }
 
-if(!function_exists('godtube_sc')) {
-
-	function godtube_sc( $atts, $content) {
-		extract(bbcodes_atts(array(
-				'width' => '',
-				'height' => '',
-				'source' => ''
-		 ), $atts));
-		 
-		$optionw = ($width !='') ? 'w='.$width : '';
-		$optionh = ($height !='') ? 'h='.$height : '';
-		$options = ($source !='') ? ' src="http://www.godtube.com/embed/source/'.$source : '';
-
-		return '<script type="text/javascript" src="'.$options.'.js?'.$optionw.'&'.$optionh.'&ap=false&sl=true&title=false"></script>'.$content;
-	}
-		
-	add_bbcodes( 'godtube', 'godtube_sc' );
-}
-
-if(!function_exists('ku6_sc')) {
-
-	function ku6_sc( $atts, $content) {
-		extract(bbcodes_atts(array(
-				'width' => '',
-				'height' => '',
-				'source' => ''
-		 ), $atts));
-		 
-		$option = ($width !='') ? ' data-width="'.$width.'"' : '';
-		$option .= ($height !='') ? ' data-height="'.$height.'"' : '';
-		$option .= ($source !='') ? ' data-vid="'.$source.'"' : '';
-
-		return '<script'.$option.' src="//player.ku6.com/out/v.js"></script>';
-	}
-		
-	add_bbcodes( 'ku6', 'ku6_sc' );
-}
-
-if(!function_exists('screenr_sc')) {
-
-	function screenr_sc( $atts, $content) {
-		extract(bbcodes_atts(array(
-				'width' => '',
-				'height' => '',
-				'source' => ''
-		 ), $atts));
-		 
-		$option = ($width !='') ? ' width="'.$width.'"' : ' width="100%"';
-		$option .= ($height !='') ? ' height="'.$height.'"' : ' height="450"';
-		$option .= ($source !='') ? ' src="//www.screenr.com/embed/'.$source.'"' : '';
-
-		return '<iframe'.$option.' frameborder="0">'.do_bbcodes($content).'</iframe>';
-	}
-		
-	add_bbcodes( 'screenr', 'screenr_sc' );
-}
 
 if(!function_exists('xvideos_sc')) {
 
@@ -407,25 +162,6 @@ if(!function_exists('xvideos_sc')) {
 	add_bbcodes( 'xvideos', 'xvideos_sc' );
 }
 
-if(!function_exists('liveleak_sc')) {
-
-	function liveleak_sc( $atts, $content) {
-		extract(bbcodes_atts(array(
-				'width' => '',
-				'height' => '',
-				'source' => ''
-		 ), $atts));
-		 
-		$option = ($width !='') ? ' width="'.$width.'"' : ' width="100%"';
-		$option .= ($height !='') ? ' height="'.$height.'"' : ' height="450"';
-		$option .= ($source !='') ? ' src="http://www.liveleak.com/ll_embed?f=2eb_'.$source.'"' : '';
-
-		return '<iframe'.$option.' frameborder="0" allowfullscreen>'.do_bbcodes($content).'</iframe>';
-	}
-		
-	add_bbcodes( 'liveleak', 'liveleak_sc' );
-}
-
 if(!function_exists('twitch_sc')) {
 
 	function twitch_sc( $atts, $content) {
@@ -443,27 +179,6 @@ if(!function_exists('twitch_sc')) {
 	}
 		
 	add_bbcodes( 'twitch', 'twitch_sc' );
-}
-
-if(!function_exists('fbvideo_sc')) {
-
-	function fbvideo_sc( $atts, $content) {
-		extract(bbcodes_atts(array(
-				'width' => '',
-				'autoplay' => '',
-				'allowfullscreen' => '',
-				'source' => ''
-		 ), $atts));
-		 
-		$option = ($width !='') ? ' data-width="'.$width.'"' : ' data-width="100%"';
-		$option .= ($autoplay !='') ? ' data-autoplay="'.$autoplay.'"' : '';
-		$option .= ($allowfullscreen !='') ? ' data-allowfullscreen="'.$allowfullscreen.'"' : ' data-allowfullscreen="true"';
-		$option .= ($source !='') ? ' data-href="https://www.facebook.com/facebook/videos/'.$source.'/"' : '';
-
-		return '<div class="fb-video"'.$option.'></div>'.$content;
-	}
-		
-	add_bbcodes( 'facebook', 'fbvideo_sc' );
 }
 
 if(!function_exists('pornhub_sc')) {
